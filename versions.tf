a39357ddc401c3e91bd41debb028f2f34157cc58
Abel Luck <abel@guardianproject.info>
2020-09-03 12:50:37 +0200

    feat!: bump required aws provider version to 3.4.0

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.0"
+      version = "~> 3.4.0"
     }
     cloudflare = {
       source  = "terraform-providers/cloudflare"
