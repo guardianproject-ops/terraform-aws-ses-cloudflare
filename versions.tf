7c49b4ebad97539b87d1b836a90baea76c61a936
Abel Luck <abel@guardianproject.info>
2021-01-27 14:45:32 +0100

    fix: Update cloudflare provider path

--- versions.tf
+++ versions.tf
@@ -6,7 +6,7 @@ terraform {
       version = "~> 3.4.0"
     }
     cloudflare = {
-      source  = "terraform-providers/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "~> 2.0"
     }
   }
